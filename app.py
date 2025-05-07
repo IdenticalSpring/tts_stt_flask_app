a8128202e98f1b2ac745caffb431eea2ebf04387
nhattlm <nhat.tlm3173@gmail.com>
2025-05-07 21:17:29 +0700

    nhatdev

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, render_template, send_file, redirect, url_for, jsonify
-from services.speech_to_textv2 import start_recording, stop_and_transcribe
 from services.task_manager import add_task
 from services.text_to_speechv2 import synth_bytes, list_voices
-
+import base64
 import uuid
 import os
 import io
@@ -13,43 +12,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
-
-
-# === Bắt đầu ghi âm ===
-@app.route("/start-record", methods=["POST"])
-def start():
-    start_recording()
-    return redirect(url_for('index'))
-
-
-# === Hàm phụ để lưu text từ STT ===
-def transcribe_and_store_to_file(task_id):
-    text_result = stop_and_transcribe()
-    with open(f"temp/text_{task_id}.txt", "w", encoding="utf-8") as f:
-        f.write(text_result)
-
-
-# === Dừng ghi âm và xử lý text ===
-@app.route("/stop-record", methods=["POST"])
-def stop():
-    task_id = str(uuid.uuid4())
-    add_task(transcribe_and_store_to_file, task_id)
-    return redirect(url_for('result', task_id=task_id))
-
-
-# === Trả kết quả STT ===
-@app.route("/result/<task_id>")
-def result(task_id):
-    text_path = f"temp/text_{task_id}.txt"
-
-    if not os.path.exists(text_path):
-        return render_template("processing.html", task_id=task_id)
-
-    with open(text_path, encoding="utf-8") as f:
-        text_result = f.read()
-
-    return render_template("result.html", text_result=text_result)
-
 
 # === TTS: Nhận văn bản, trả về .wav (stream) ===
 @app.route("/tts", methods=["POST"])
@@ -67,13 +29,12 @@ def tts():
             return jsonify({"status": "error", "message": "Missing text input"}), 400
 
         wav_bytes = synth_bytes(text, voice)
-
-        return send_file(
-            io.BytesIO(wav_bytes),
-            mimetype="audio/wav",
-            as_attachment=False,
-            download_name="tts_output.wav"
-        )
+        base64_audio = base64.b64encode(wav_bytes).decode("utf-8")
+        return jsonify({
+            "status": "success",
+            "audioData": base64_audio,
+            "voiceID": voice
+        })
 
     except Exception as e:
         import traceback
